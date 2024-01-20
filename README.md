aba25f3af5588b2e106d96a8d33c6e27109126d6
Tajpc <157214005+Tajpc@users.noreply.github.com>
2024-01-20 17:46:55 +0330

    -m "start from the begining" git commit -m "start from the begining" i --INSERT-- ] start from the begining

--- README.md
+++
@@ -1,2 +0,0 @@
-# BP_SUT_Projects
-my codes are here.
--- pat2.py
+++
@@ -1,8 +0,0 @@
-from re import findall
-n = int(input())
-email_list = [input() for item in range(n)]
-domain_list = []
-for i in email_list:
-    domain_list.append(str(findall("@[a-z]+.[a-z]+", i)))
-for x in sorted(set(domain_list)):
-    print(x[3:-2])
