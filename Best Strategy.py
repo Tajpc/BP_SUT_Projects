508119cda494ba43bfcf3c697e1940b9fe9b9a06
Tajpc <157214005+Tajpc@users.noreply.github.com>
2024-01-21 13:52:53 +0330

    changed best strategy

--- Best Strategy.py
+++ Best Strategy.py
@@ -1,16 +1,26 @@
-def printpascal(n):
-    arr = [1]
-    temp = []
-    for i in range(n):
-        for j in range(len(arr)):
-            print(arr[j], end= " ")
-        print()
-        temp.append(1)
-        for j in range(len(arr)-1):
-            temp.append(arr[j] + arr[j+1])
-        temp.append(1)
-        arr = temp
-        temp = []
+def isprime(n):
+    if n <= 1:
+        return False
+    for item in range(2, int(n/2)+1):
+            if (n % item) == 0:
+                return False
+    return True
+def prime_counter(a, b):
+    s = 0
+    if a < b :
+        for i in range(a, b+1):
+            if isprime(i) == True:
+                s += 1
+    else:
+        for i in range(b, a+1):
+            if isprime(i) == True:
+                s += 1
+    return s
 
-n = int(input())
-printpascal(n)
+
+a , b = map(int, input(). split())
+x = prime_counter(a, b)
+if a <= b :
+    print("main order - amount: ", x)
+else:
+    print("reverse order - amount: ", x)
